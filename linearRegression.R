934666a7598ed4d6e373f87332b2e68be9e13757
Rafal Stepien <rstepien095@gmail.com>
2020-03-23 22:26:58 +0100

    Add stable version of working linear_model() function

--- linearRegression.R
+++ linearRegression.R
@@ -18,10 +18,24 @@ get_stars <- function(p.val.vector){
   stars_vector
 }
 
+unpack_names <- function(names_list){
+  new_names_vect <- c()
+  new_names_vect[1] <- "(Intercept)"
+  for (i in 1:length(names_list)){
+    new_names_vect[i+1] <- as.character(names_list[[i]])
+  }
+  new_names_vect
+}
+
 linear_model = function(data, dependent_variable, ...) {
-  independents = list(...)
+  
+  # ARGUMENTS CONFIG
+  args <- as.list(match.call())
+  args_n <- length(as.list(match.call()))
+  independents <- unpack_names(args[4:args_n])
+  
   nrows = length(dependent_variable)
-  ncols = length(independents)
+  ncols = length(independents)-1
   X=matrix(1, nrows, ncols+1)
   for (i in 1:ncols+1){
     X[, i] <- data[, i]
@@ -34,23 +48,22 @@ linear_model = function(data, dependent_variable, ...) {
   D = S2e*solve(t(X)%*%X)
   Salpha = sqrt(diag(D))
   testT = alpha/Salpha
-  print(testT)
   p.val = 2*pt(abs(testT), nrows-ncols-1, lower.tail=FALSE)
-  print(p.val)
   R2 = 1 - sum(res^2)/sum((y-mean(y))^2)
   F_stat = (R2/(1-R2)) * (nrows-ncols-1)/(ncols)
-  F_stat_p.val = pf(F, ncols, nrows-ncols-1, lower.tail=FALSE)
+  F_stat_p.val = pf(F_stat, ncols, nrows-ncols-1, lower.tail=FALSE)
   names <-  as.vector(match.call(expand.dots = FALSE)$...)
-  cat("Call:\nformula = ", deparse(substitute(dependent_variable)), "~", names[[1]], "+",  names[[2]], "+", names[[3]])
   cat("\n\nResiduals:\n", summary(res))
   cat("\n\nCoefficients:\n")
-  df <- data.frame(X=c("(Intercept)", as.character(names[[1]]), as.character(names[[2]]), as.character(names[[3]])),
+  df <- data.frame(X=independents,
                    Estimate.Std=as.vector(alpha),
                    Std.Error=as.vector(Salpha),
                    t.value=as.vector(testT),
                    Pr=as.vector(p.val),
                    stars=get_stars(as.vector(p.val)))
-  format(df, justify="left", digit=2)
+  print(format(df, justify="centre", digit=2))
+  cat("---\nSignif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1\n")
+  cat("\nResidual standard error:", round(sqrt(S2e), 4),  "on", nrows-ncols-1,  "degrees of freedom\nMultiple R-squared::", round(R2, 4), "\nF-statistic:", F_stat, "on", ncols, "and", nrows-ncols-1, "DF, F-statistics p-value:", F_stat_p.val)
 }
 
 linear_model(data, dependent_variable=Sepal.Length, Sepal.Width, Petal.Length, Petal.Width)
